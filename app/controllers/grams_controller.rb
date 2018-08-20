025a36cee1e455fb1441c54f90c822c9438ea760
Your Name <tina.yuting.lee@gmail.com>
2018-08-20 23:03:39 +0000

    Hooked up edit function for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -19,9 +19,27 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end   
+    return render_not_found if @gram.blank?  
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    #return statement exits function without rest of code triggering
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
   end
 
 private
@@ -29,5 +47,11 @@ private
   def gram_params
     params.require(:gram).permit(:message)
   end
+ 
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
+  end
+
 end
 
