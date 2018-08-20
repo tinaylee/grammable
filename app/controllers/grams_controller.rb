14127eb6e211ce8a482509b6fa2ca5b6a70b0f7b
Your Name <tina.yuting.lee@gmail.com>
2018-08-20 21:55:04 +0000

    Hooked up show page for individual grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -17,6 +17,14 @@ class GramsController < ApplicationController
   def index
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end   
+  end
+
+private
 
   def gram_params
     params.require(:gram).permit(:message)
